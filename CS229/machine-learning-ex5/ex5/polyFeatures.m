6aae3f8f3dc7f22067cca9ac841b3c9cfad09d07
closada <losadalastra.carlos@gmail.com>
2017-06-25 18:21:54 +0100

    finished w6 & w8

--- CS229/machine-learning-ex5/ex5/polyFeatures.m
+++ CS229/machine-learning-ex5/ex5/polyFeatures.m
@@ -10,12 +10,13 @@ function [X_poly] = polyFeatures(X, p)
 X_poly = zeros(numel(X), p);
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Given a vector X, return a matrix X_poly where the p-th
+% Instructions: Given a vector X, return a matrix X_poly where the p-th 
 %               column of X contains the values of X to the p-th power.
 %
-%
-for n=1:p
-    X_poly(:,n) = X.^n;
+% 
+
+for i = 1:p
+  X_poly(:,i) = X(:,1).^i;
 end
 
 % =========================================================================
